4f3247a199a76b8e9afa002631a691ccf4b55b0b
Jason Aviles <sandman2k18@outlook.com>
2019-11-08 13:59:42 -0500

    tested done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,4 +7,13 @@ def count_th(word):
     
     # TBC
     
-    pass
+   i = word.find("th")  #Find first orruance of "th" in word
+
+   if i == -1: #if it does not occur, then there are no more "th" in rest of word. Return 0
+        return 0
+
+    # We have found first occurance of "th" starting at index i. So we have 1
+    # So now call this function again with rest of word starting from i + 2 (length of "th")
+        return 1 + count_th(word[i+2:])
+
+print(count_th("abcthefthghith"))
